79de65e21339d02ba6ab3304716b29cbfbf61e61
Gerry Sousa <gerry.nett@gmail.com>
2018-11-11 16:12:23 -0200

    Correções gitignore

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -18,24 +18,20 @@ end
 
 After do |scenario|
   tira_print(scenario)
-
   tem = page.has_css?('#userMenu')
 
   if tem
     visit '/sair'
     wait_until_css('.swal2-modal')
-  end
-
-  
+  end  
 end
 
 def tira_print(scenario)
-  nome = scenario.name.gsub(/^A-Za-Z0-9/,'')
-  nome = nome.tr(' ','_').downcase!
-  screenshot = "/log/screenshot/#{nome}.png"
-
+  nome = scenario.name.gsub(/[^A-Za-z0-9 ]/, '')
+  nome = nome.tr(' ', '_').downcase!
+  screenshot = "log/screenshots/#{nome}.png"
   page.save_screenshot(screenshot)
-  embed(screenshot, '/image/png', 'Ta aqui o print ;)')
+  embed(screenshot, 'image/png', 'Ta aqui o print :)')
 end
 
 # After('@logout') do
